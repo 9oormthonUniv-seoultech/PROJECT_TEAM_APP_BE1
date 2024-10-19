2523759b93ba18087f378df4c98efdc92b223cda
orieasy1 <easy1nhard2@gmail.com>
2024-10-20 00:04:14 +0900

    Feat: 강의실 모고록 조회 API 개발

--- src/main/java/com/groomiz/billage/classroom/repository/ClassroomRepository.java
+++ src/main/java/com/groomiz/billage/classroom/repository/ClassroomRepository.java
@@ -1,6 +1,7 @@
 package com.groomiz.billage.classroom.repository;
 
 import java.time.LocalDate;
+import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -9,5 +10,8 @@ import org.springframework.data.repository.query.Param;
 import com.groomiz.billage.classroom.entity.Classroom;
 
 public interface ClassroomRepository extends JpaRepository<Classroom, Long> {
-
+	@Query("SELECT c FROM Classroom c WHERE c.building.id = :buildingId AND c.floor = :floor AND c.capacity >= :capacity")
+	List<Classroom> findByCondition(@Param("buildingId") Long buildingId,
+		@Param("floor") Long floor,
+		@Param("capacity") int capacity);
 }
--- src/main/java/com/groomiz/billage/reservation/repository/ReservationRepository.java
+++ src/main/java/com/groomiz/billage/reservation/repository/ReservationRepository.java
@@ -1,6 +1,7 @@
 package com.groomiz.billage.reservation.repository;
 
 import java.time.LocalDate;
+import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -9,5 +10,5 @@ import org.springframework.data.repository.query.Param;
 import com.groomiz.billage.reservation.entity.Reservation;
 
 public interface ReservationRepository extends JpaRepository<Reservation, Long> {
-
+	List<Reservation> findByClassroomIdAndApplyDate(Long classroomId, LocalDate applyDate);
 }
